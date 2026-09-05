9f9087aabf881f5412464c2a19a560a51af485b3
Noppakaow Matwanna <noppakaowmt@gmail.com>
2026-09-05 19:21:58 +0700

    fix production socket connection

--- foxyposapp/src/screens/kitchen/kitchenSocket.js
+++ foxyposapp/src/screens/kitchen/kitchenSocket.js
@@ -1,13 +1,11 @@
 import { io } from "socket.io-client";
 import { API_ORIGIN } from "../../services/Api";
 
-
 const socket = io(API_ORIGIN, {
-  transports: ["websocket"],
+  transports: ["websocket", "polling"],
   autoConnect: true,
   reconnection: true,
   reconnectionAttempts: Infinity,
 });
-
 
 export default socket;
